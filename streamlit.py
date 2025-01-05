af72e6c7455eb5a21020373ed81695eef7c48df6
ppison1 <164343273+ppison1@users.noreply.github.com>
2025-01-05 14:02:05 +0100

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,58 @@ import requests
 from xml.etree import ElementTree as ET
 from bs4 import BeautifulSoup
 import dateutil.parser
+from datetime import datetime, timedelta
 import pytz
+
+
+user = st.secrets["user"]
+passwd = st.secrets["password"]
+
+# List of RSS feeds
+RSS_FEEDS = {
+    "Calendario economico": None,
+    "Investing.com Più Rilevanti": "https://it.investing.com/rss/news_285.rss",
+    "Investing.com Economia": "https://it.investing.com/rss/news_14.rss",
+    "Investing.com Mercato azionario": "https://it.investing.com/rss/news_25.rss",
+    "Il sole 24 ore Italia": "https://www.ilsole24ore.com/rss/italia.xml",
+    "Il sole 24 ore Mondo": "https://www.ilsole24ore.com/rss/mondo.xml",
+    "Il sole 24 ore Economia": "https://www.ilsole24ore.com/rss/economia.xml",
+    "Il sole 24 ore Finanza": "https://www.ilsole24ore.com/rss/finanza.xml",
+    # "Corriere Esteri": "https://www.corriere.it/dynamic-feed/rss/section/Esteri.xml",
+    # "Corriere Economia": "https://www.corriere.it/dynamic-feed/rss/section/Economia.xml",
+    # "Corriere Scienza": "https://www.corriere.it/dynamic-feed/rss/section/Scienza.xml",
+    "Corriere Motori": "https://xml2.corriereobjects.it/rss/motori.xml",
+    "Corriere Milano": "https://www.corriere.it/dynamic-feed/rss/section/Milano.xml",
+    "F1": "https://it.motorsport.com/rss/f1/news/",
+    "MotoGP": "https://www.moto.it/rss/news-motogp.xml",
+    "Focus Tecnologia": "https://www.focus.it/rss/tecnologia.rss",
+    "Focus Comportamento": "https://www.focus.it/rss/comportamento.rss",
+    "Focus Scienza": "https://www.focus.it/rss/scienza.rss",
+}
+
+
+def login():
+    st.title("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if username == user and password == passwd:
+            st.session_state["authenticated"] = True
+            st.session_state["last_activity"] = datetime.now()
+            st.success("Login successful!")
+        else:
+            st.error("Invalid username or password")
+
+
+# Check for inactivity
+def check_inactivity():
+    if "last_activity" in st.session_state:
+        now = datetime.now()
+        if now - st.session_state["last_activity"] > timedelta(minutes=5):
+            st.session_state["authenticated"] = False
+            st.sidebar.warning("You have been logged out due to inactivity.")
+    st.session_state["last_activity"] = datetime.now()
+
 
 # Function to fetch articles from an RSS feed
 def fetch_rss_articles(feed_url):
@@ -39,75 +90,67 @@ def fetch_rss_articles(feed_url):
                 pass
     return articles
 
-# List of RSS feeds
-RSS_FEEDS = {
-    "Calendario economico": None,
-    "Investing.com Più Rilevanti": "https://it.investing.com/rss/news_285.rss",
-    "Investing.com Economia": "https://it.investing.com/rss/news_14.rss",
-    "Investing.com Mercato azionario": "https://it.investing.com/rss/news_25.rss",
-    "Il sole 24 ore Italia": "https://www.ilsole24ore.com/rss/italia.xml",
-    "Il sole 24 ore Mondo": "https://www.ilsole24ore.com/rss/mondo.xml",
-    "Il sole 24 ore Economia": "https://www.ilsole24ore.com/rss/economia.xml",
-    "Il sole 24 ore Finanza": "https://www.ilsole24ore.com/rss/finanza.xml",
-    # "Corriere Esteri": "https://www.corriere.it/dynamic-feed/rss/section/Esteri.xml",
-    # "Corriere Economia": "https://www.corriere.it/dynamic-feed/rss/section/Economia.xml",
-    # "Corriere Scienza": "https://www.corriere.it/dynamic-feed/rss/section/Scienza.xml",
-    "Corriere Motori": "https://xml2.corriereobjects.it/rss/motori.xml",
-    "Corriere Milano": "https://www.corriere.it/dynamic-feed/rss/section/Milano.xml",
-    "F1": "https://it.motorsport.com/rss/f1/news/",
-    "MotoGP": "https://www.moto.it/rss/news-motogp.xml",
-    "Focus Tecnologia": "https://www.focus.it/rss/tecnologia.rss",
-    "Focus Comportamento": "https://www.focus.it/rss/comportamento.rss",
-    "Focus Scienza": "https://www.focus.it/rss/scienza.rss",
-}
 
-# Streamlit app layout
-st.title("RSS Feed Viewer")
+# Check login status
+if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
+    login()
+else:
+    check_inactivity()
 
-# feed_name = st.sidebar.selectbox("Select an RSS Feed", list(RSS_FEEDS.keys()))
-feed_name = st.sidebar.radio(
-    "Select an RSS Feed",
-    list(RSS_FEEDS.keys()),
-    index=None,
-)
-if feed_name:
-    if feed_name == "Calendario economico":
-        st.write("### Economic Calendar")
-        st.components.v1.html("""
-            <iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=2,3&features=datepicker,timezone&countries=5&calType=day&timeZone=16&lang=9" width="650" height="467" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
-            <div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">
-                <span style="font-size: 11px;color: #333333;text-decoration: none;">
-                    Calendario economico fornito da 
-                    <a href="https://it.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com Italia</a> 
-                    - Il Portale di Trading sul Forex e sui titoli di borsa.
-                </span>
-            </div>
-        """, height=500)
+    if not st.session_state["authenticated"]:
+        st.stop()
 
-    else:
-        feed_url = RSS_FEEDS[feed_name]
+    if st.sidebar.button("Logout"):
+        st.session_state["authenticated"] = False
 
-        st.write(f"### Articles from {feed_name}")
+    # Streamlit app layout
+    st.title("NEWS")
 
-        # Fetch and display articles
-        articles = fetch_rss_articles(feed_url)
-        sorted_articles = sorted(articles, key=lambda x: x['pub_date'], reverse=True)
+    # feed_name = st.sidebar.selectbox("Select an RSS Feed", list(RSS_FEEDS.keys()))
+    feed_name = st.sidebar.radio(
+        "Select an RSS Feed",
+        list(RSS_FEEDS.keys()),
+        index=None,
+    )
 
-        for article in sorted_articles:
-            col1, col2 = st.columns([10, 1])
-            with col1:
-                st.write(f"#### [{article['title']}]({article['link']})")
-                st.write(f"Published on: {article['pub_date']}")
-                st.write(article['description'])
-                # st.write(article['clean_link'])
-            with col2:
-                if "ilsole24ore" in feed_name.replace(" ", "").lower() or "corriere" in feed_name.replace(" ", "").lower():
-                    copy_button_html = f"""
-                    <button onclick="navigator.clipboard.writeText('Riassumi in italiano {article['clean_link']}')">Copy</button>
-                    """
-                else:
-                    copy_button_html = f"""
-                    <button onclick="navigator.clipboard.writeText('Riassumi in italiano {article['link']}')">Copy</button>
-                    """
-                st.components.v1.html(copy_button_html, height=35)
-            st.write("---")
+    if feed_name:
+        if feed_name == "Calendario economico":
+            st.write("### Economic Calendar")
+            st.components.v1.html("""
+                <iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=2,3&features=datepicker,timezone&countries=5&calType=day&timeZone=16&lang=9" width="650" height="467" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
+                <div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">
+                    <span style="font-size: 11px;color: #333333;text-decoration: none;">
+                        Calendario economico fornito da 
+                        <a href="https://it.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com Italia</a> 
+                        - Il Portale di Trading sul Forex e sui titoli di borsa.
+                    </span>
+                </div>
+            """, height=500)
+
+        else:
+            feed_url = RSS_FEEDS[feed_name]
+
+            st.write(f"### Articles from {feed_name}")
+
+            # Fetch and display articles
+            articles = fetch_rss_articles(feed_url)
+            sorted_articles = sorted(articles, key=lambda x: x['pub_date'], reverse=True)
+
+            for article in sorted_articles:
+                col1, col2 = st.columns([10, 1])
+                with col1:
+                    st.write(f"#### [{article['title']}]({article['link']})")
+                    st.write(f"Published on: {article['pub_date']}")
+                    st.write(article['description'])
+                    # st.write(article['clean_link'])
+                with col2:
+                    if "ilsole24ore" in feed_name.replace(" ", "").lower() or "corriere" in feed_name.replace(" ", "").lower():
+                        copy_button_html = f"""
+                        <button onclick="navigator.clipboard.writeText('Riassumi in italiano {article['clean_link']}')">Copy</button>
+                        """
+                    else:
+                        copy_button_html = f"""
+                        <button onclick="navigator.clipboard.writeText('Riassumi in italiano {article['link']}')">Copy</button>
+                        """
+                    st.components.v1.html(copy_button_html, height=35)
+                st.write("---")
