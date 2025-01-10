12533a006873333025a92a21c8ff334229722063
ppison1 <164343273+ppison1@users.noreply.github.com>
2025-01-10 11:52:09 +0100

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -14,25 +14,33 @@ g_id = st.secrets["g_id"]
 timeout = int(st.secrets["timeout"])
 
 # List of RSS feeds
+# RSS_FEEDS = {
+#     "Calendario economico": None,
+#     "Investing.com Più Rilevanti": "https://it.investing.com/rss/news_285.rss",
+#     "Investing.com Economia": "https://it.investing.com/rss/news_14.rss",
+#     "Investing.com Mercato azionario": "https://it.investing.com/rss/news_25.rss",
+#     "Il sole 24 ore Italia": "https://www.ilsole24ore.com/rss/italia.xml",
+#     "Il sole 24 ore Mondo": "https://www.ilsole24ore.com/rss/mondo.xml",
+#     "Il sole 24 ore Economia": "https://www.ilsole24ore.com/rss/economia.xml",
+#     "Il sole 24 ore Finanza": "https://www.ilsole24ore.com/rss/finanza.xml",
+#     # "Corriere Esteri": "https://www.corriere.it/dynamic-feed/rss/section/Esteri.xml",
+#     # "Corriere Economia": "https://www.corriere.it/dynamic-feed/rss/section/Economia.xml",
+#     # "Corriere Scienza": "https://www.corriere.it/dynamic-feed/rss/section/Scienza.xml",
+#     "Corriere Motori": "https://xml2.corriereobjects.it/rss/motori.xml",
+#     "Corriere Milano": "https://www.corriere.it/dynamic-feed/rss/section/Milano.xml",
+#     "F1": "https://it.motorsport.com/rss/f1/news/",
+#     "MotoGP": "https://www.moto.it/rss/news-motogp.xml",
+#     "Focus Tecnologia": "https://www.focus.it/rss/tecnologia.rss",
+#     "Focus Comportamento": "https://www.focus.it/rss/comportamento.rss",
+#     "Focus Scienza": "https://www.focus.it/rss/scienza.rss",
+# }
 RSS_FEEDS = {
-    "Calendario economico": None,
-    "Investing.com Più Rilevanti": "https://it.investing.com/rss/news_285.rss",
-    "Investing.com Economia": "https://it.investing.com/rss/news_14.rss",
-    "Investing.com Mercato azionario": "https://it.investing.com/rss/news_25.rss",
-    "Il sole 24 ore Italia": "https://www.ilsole24ore.com/rss/italia.xml",
-    "Il sole 24 ore Mondo": "https://www.ilsole24ore.com/rss/mondo.xml",
-    "Il sole 24 ore Economia": "https://www.ilsole24ore.com/rss/economia.xml",
-    "Il sole 24 ore Finanza": "https://www.ilsole24ore.com/rss/finanza.xml",
-    # "Corriere Esteri": "https://www.corriere.it/dynamic-feed/rss/section/Esteri.xml",
-    # "Corriere Economia": "https://www.corriere.it/dynamic-feed/rss/section/Economia.xml",
-    # "Corriere Scienza": "https://www.corriere.it/dynamic-feed/rss/section/Scienza.xml",
-    "Corriere Motori": "https://xml2.corriereobjects.it/rss/motori.xml",
-    "Corriere Milano": "https://www.corriere.it/dynamic-feed/rss/section/Milano.xml",
-    "F1": "https://it.motorsport.com/rss/f1/news/",
-    "MotoGP": "https://www.moto.it/rss/news-motogp.xml",
-    "Focus Tecnologia": "https://www.focus.it/rss/tecnologia.rss",
-    "Focus Comportamento": "https://www.focus.it/rss/comportamento.rss",
-    "Focus Scienza": "https://www.focus.it/rss/scienza.rss",
+    "Calendario": None,
+    "Mondo": ["https://it.investing.com/rss/news_285.rss", "https://www.ilsole24ore.com/rss/mondo.xml"],
+    "Economia": ["https://it.investing.com/rss/news_14.rss", "https://www.ilsole24ore.com/rss/economia.xml"],
+    "Finanza": ["https://it.investing.com/rss/news_25.rss", "https://www.ilsole24ore.com/rss/finanza.xml"],
+    "Italia": ["https://www.ilsole24ore.com/rss/italia.xml", "https://www.corriere.it/dynamic-feed/rss/section/Milano.xml"],
+    "Motori": ["https://xml2.corriereobjects.it/rss/motori.xml", "https://it.motorsport.com/rss/f1/news/", "https://www.moto.it/rss/news-motogp.xml"],
 }
 
 
@@ -60,37 +68,39 @@ def check_inactivity():
 
 
 # Function to fetch articles from an RSS feed
-def fetch_rss_articles(feed_url):
-    response = requests.get(feed_url)
-    response.raise_for_status()
-    root = ET.fromstring(response.content)
-
+def fetch_rss_articles(feed_urls):
     articles = []
-    for channel in root.findall("channel"):
-        for item in channel.findall("item"):
-            try:
-                title = item.find("title").text
-                link = item.find("link").text
+
+    for feed_url in feed_urls:
+        response = requests.get(feed_url)
+        response.raise_for_status()
+        root = ET.fromstring(response.content)
+
+        for channel in root.findall("channel"):
+            for item in channel.findall("item"):
                 try:
-                    description = item.find("description").text
-                except:
-                    description = ""
-                pub_date = item.find("pubDate").text
-                pub_date = dateutil.parser.parse(pub_date).astimezone(pytz.timezone('Europe/Rome'))
+                    title = item.find("title").text
+                    link = item.find("link").text
+                    try:
+                        description = item.find("description").text
+                    except:
+                        description = ""
+                    pub_date = item.find("pubDate").text
+                    pub_date = dateutil.parser.parse(pub_date).astimezone(pytz.timezone('Europe/Rome'))
 
-                # Clean up description using BeautifulSoup
-                soup = BeautifulSoup(description, 'html.parser')
-                description_text = soup.get_text()
+                    # Clean up description using BeautifulSoup
+                    soup = BeautifulSoup(description, 'html.parser')
+                    description_text = soup.get_text()
 
-                articles.append({
-                    "title": title,
-                    "link": link,
-                    "clean_link": f"https://12ft.io/{link}",
-                    "description": description_text,
-                    "pub_date": pub_date.strftime("%Y-%m-%d %H:%M:%S"),
-                })
-            except Exception as e:
-                pass
+                    articles.append({
+                        "title": title,
+                        "link": link,
+                        "clean_link": f"https://12ft.io/{link}",
+                        "description": description_text,
+                        "pub_date": pub_date.strftime("%Y-%m-%d %H:%M:%S"),
+                    })
+                except Exception as e:
+                    pass
     return articles
 
 
@@ -157,12 +167,12 @@ else:
             """, height=500)
 
         else:
-            feed_url = RSS_FEEDS[feed_name]
+            feed_urls = RSS_FEEDS[feed_name]
 
             st.write(f"### Articles from {feed_name}")
 
             # Fetch and display articles
-            articles = fetch_rss_articles(feed_url)
+            articles = fetch_rss_articles(feed_urls)
             sorted_articles = sorted(articles, key=lambda x: x['pub_date'], reverse=True)
 
             for article in sorted_articles:
@@ -175,7 +185,7 @@ else:
                     output_placeholder = st.empty()
                 with col2:
                     if st.button(f"AI", key=article['link']):
-                        if "ilsole24ore" in feed_name.replace(" ", "").lower() or "corriere" in feed_name.replace(" ", "").lower():
+                        if "ilsole24ore" in article['link'] or "corriere" in article['link']:
                             output = process_link(article['clean_link'])
                         else:
                             output = process_link(article['link'])
