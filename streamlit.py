f5158402d05928dfbefc1bdf2f7d5f87f05662bd
ppison1 <164343273+ppison1@users.noreply.github.com>
2025-04-08 15:10:20 -0400

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,7 @@ import google.generativeai as genai
 from tvDatafeed import TvDatafeed, Interval
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import io
 import re
 from extra_streamlit_components import CookieManager
@@ -24,7 +25,7 @@ user = st.secrets["user"]
 passwd = st.secrets["password"]
 
 RSS_FEEDS = {
-    # "Chart": None,
+    "Pnl": None,
     "Calendario": None,
     "Mondo": ["https://it.investing.com/rss/news_285.rss", "https://www.ilsole24ore.com/rss/mondo.xml"],
     "Economia": ["https://it.investing.com/rss/news_14.rss", "https://www.ilsole24ore.com/rss/economia.xml", "https://feeds.content.dowjones.io/public/rss/socialeconomyfeed", "https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness"],
@@ -121,6 +122,45 @@ else:
         if feed_name == "Calendario":
             st.write("### Economic Calendar")
             st.components.v1.html("""<iframe src="https://sslecal2.investing.com?columns=exc_importance&importance=2,3&features=datepicker,timeselector&countries=5&calType=day&timeZone=8&lang=9" width="475" height="467" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;"><span style="font-size: 11px;color: #333333;text-decoration: none;">Calendario economico fornito da <a href="https://it.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com Italia</a> - Il Portale di Trading sul Forex e sui titoli di borsa.</span></div>""", height=600)
+        elif feed_name == "Pnl":
+            st.write("### Analisi del PNL")
+            uploaded_file = st.file_uploader("Carica il tuo file CSV", type=["csv"])
+            if uploaded_file is not None:
+                df_merged = pd.read_csv(uploaded_file, index_col=0)
+                st.subheader("Tabella degli ultimi 10 record")
+                st.dataframe(df_merged[::-1])
+                fig = make_subplots(
+                    rows=2, cols=1,
+                    shared_xaxes=True,
+                    vertical_spacing=0.1,
+                    subplot_titles=("Total USD Cumulative", "Bar Chart: Total Realized PNL")
+                )
+                fig.add_trace(
+                    go.Scatter(
+                        x=df_merged['Trade Date'],
+                        y=df_merged['Total USD Cumulative'],
+                        mode='lines',
+                        name='Cumulative PNL'
+                    ),
+                    row=1, col=1
+                )
+                fig.add_trace(
+                    go.Bar(
+                        x=df_merged['Trade Date'],
+                        y=df_merged['Total Realized PNL'],
+                        name='Daily PNL',
+                        marker_color=df_merged['Total Realized PNL'].apply(lambda x: 'green' if x > 0 else 'red')
+                    ),
+                    row=2, col=1
+                )
+                fig.update_xaxes(type='category', categoryorder='array', categoryarray=df_merged['Trade Date'], showticklabels=False)
+                fig.update_layout(
+                    title_text="Total USD Cumulative & Daily PNL",
+                    height=800,
+                )
+                st.subheader("Grafico: Total USD Cumulative & Daily PNL")
+                st.plotly_chart(fig)
+        
         elif feed_name == "Chart":
             st.write("### Charts")
             tv = TvDatafeed()
