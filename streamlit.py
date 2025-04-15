2fe25449ce82198b0d3f86b584b950cc62dcb244
ppison1 <164343273+ppison1@users.noreply.github.com>
2025-04-15 18:54:21 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -237,12 +237,12 @@ else:
                     copy_button_html = f"""
                     <button 
                       onclick="navigator.clipboard.writeText('{title}')" 
-                      style="color: #1f77b4; font-size: 20px; font-weight: bold; background: none; border: none; cursor: pointer;"
+                      style="color: #1f77b4; font-size: 20px; font-weight: bold; background: none; border: none; cursor: pointer; padding: 5px 10px; line-height: 1.2; width: auto; white-space: normal;"
                     >
                       {article['title']}
                     </button>
                     """
-                    st.components.v1.html(copy_button_html, height=55)
+                    st.components.v1.html(copy_button_html, height=0)
                     #st.write(f"<span style='color:#1f77b4; font-size: 20px; font-weight: bold;'>{article['title']}</span>", unsafe_allow_html=True)
                     st.write(f"Published on: {article['pub_date']} - {article['source']}")
                     #with col2:
